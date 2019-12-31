787e8cc5b176e38cc92078f7b874c7ee3931382f
tamotsu ichiba <ichibat@gmail.com>
2019-12-31 14:09:04 +0900

    improved

--- nuxt.config.js
+++ nuxt.config.js
@@ -58,4 +58,21 @@ export default {
     extend(config, ctx) {
     }
   }
+
+// ユーザー認証
+
+auth: {
+  strategies: {
+    local: {
+      endpoints: {
+        login: { url: '/api/auth/login', method: 'post', propertyName: 'token' },
+        logout: { url: '/api/auth/logout', method: 'post' },
+        user: { url: '/api/auth/user', method: 'get', propertyName: 'user' }
+      },
+      // tokenRequired: true,
+      // tokenType: 'bearer'
+    }
+  }
+}
+
 }
